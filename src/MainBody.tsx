7dab5c7c6facf9ec811e4f01633794a93ab20c41
cgpinta <pinta.christian@outlook.com>
2025-07-23 17:15:38 -0500

    initial design

--- src/MainBody.tsx
+++ src/MainBody.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import ImgSeq from './ImageSequence';
-import SectionSelector from './SectionSelector';
+import GridItem from './GridItem';
+import iconDocument from './images/document.png';
+import iconGithub from './images/github-mark.png';
 
 function MainBody(){
     return(
-        <section className="MainBody">
-            <section className="MainCenterSection">
-                <ImgSeq />
-                <SectionSelector />
+        <section className='Background'>
+            <section className="MainBody">
+                <h1>Christian Pinta</h1>
+                <section className="MainGrid">
+                    <GridItem title="Github" image={iconGithub} link='https://github.com/cpinta' newTab={true} />
+                    <GridItem title="Resume" image={iconDocument} imageStyle={{filter: 'invert(1)'}} imageAlt='Document icons created by Freepik - Flaticon' link='' />
+                </section>
+                <section>
+                    <p>
+                    </p>
+                </section>
             </section>
         </section>
     )
