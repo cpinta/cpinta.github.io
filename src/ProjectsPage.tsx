1649b218d1e6dcc9ccbe7fd4aa975749016c6749
cgpinta <pinta.christian@outlook.com>
2025-07-31 23:35:36 -0500

    tweaked A Skeleton Stole My Wife description

--- src/ProjectsPage.tsx
+++ src/ProjectsPage.tsx
@@ -8,6 +8,8 @@ import gifGuy from './images/guy.gif';
 
 import ProjectListEntry from './ProjectListEntry';
 
+var assmyDesc: string = 'Created a game in the span of a month using a specific theme. Effectively collaborated with an artist in order to create designs and animations for the project. Finished the project, obeying the deadline, and submitted it to a game jam event.';
+
 function ProjectsPage(){
     return(
         <>
@@ -15,7 +17,7 @@ function ProjectsPage(){
             {/* <h2>Projects</h2>  */}
             {/* bold only parts of the description text */}
             <ProjectListEntry title='PintoBoy' subtitle='C#, Unity, Blender' date='12/2023 - 02/2024' link='https://github.com/cpinta/PintoBoy-LethalCompanyMod' image={imgPintoBoy} hover={gifPintoBoy} descrption='Programmed, 3D modeled, and textured an item for the game Lethal Company. The item was published on a public modding website where I received and responded to feedback. The item has amassed over 800,000 downloads.' />
-            <ProjectListEntry title='A Skeleton Stole My Wife' subtitle='GDScript, Godot' date='10/2024' link='https://github.com/cpinta/a-skeleton-stole-my-wife' image={imgGuy} hover={gifGuy} descrption='Created a game in the span of a month using a specific theme. Used effective communication with an artist in order to create designs and animations for the project. Was able to finish the project on time and submitted it to a game jam event.' />
+            <ProjectListEntry title='A Skeleton Stole My Wife' subtitle='GDScript, Godot' date='10/2024' link='https://github.com/cpinta/a-skeleton-stole-my-wife' image={imgGuy} hover={gifGuy} descrption={assmyDesc} />
 
         </>
     )
