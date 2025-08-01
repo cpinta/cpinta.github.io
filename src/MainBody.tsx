ae95afd18f3c58ba8035dddf475f16d66529dfd3
cgpinta <pinta.christian@outlook.com>
2025-07-31 23:35:13 -0500

    moved TinyPromoBubbles to a more consistent position

--- src/MainBody.tsx
+++ src/MainBody.tsx
@@ -20,16 +20,16 @@ function MainBody(){
                 <BGStars />
                 <div style={{zIndex: 5, pointerEvents: 'none'}}>
                     <h1 className='MainBodyTitle'>Christian Pinta</h1>
-                    <div className='TinyPromoBubblesParent'>
-                        <div className="TinyPromoBubbles" style={{float: 'left', height: '3rem'}}>
-                            <div className="MainGrid" style={{width: '15rem'}} >
-                                <GridItem title="Github" image={iconGithub} link='https://github.com/cpinta' newTab={true} />
-                                <GridItem title="LinkedIn" image={iconLinkedIn} link='https://www.linkedin.com/in/christian-pinta/' newTab={true} />
-                                <GridItem title="Resume" image={iconDocument} link='./Resume.pdf' newTab={true} imageStyle={{filter: 'invert(1)'}} imageAlt='Document icons created by Freepik - Flaticon' />
+                    <div className="ProjectBody" style={{pointerEvents: 'all'}}>
+                        <div className='TinyPromoBubblesParent'>
+                            <div className="TinyPromoBubbles" style={{float: 'left', height: '3rem'}}>
+                                <div className="MainGrid" style={{width: '15rem'}} >
+                                    <GridItem title="Github" image={iconGithub} link='https://github.com/cpinta' newTab={true} />
+                                    <GridItem title="LinkedIn" image={iconLinkedIn} link='https://www.linkedin.com/in/christian-pinta/' newTab={true} />
+                                    <GridItem title="Resume" image={iconDocument} link='./Resume.pdf' newTab={true} imageStyle={{filter: 'invert(1)'}} imageAlt='Document icons created by Freepik - Flaticon' />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="ProjectBody" style={{pointerEvents: 'all'}}>
                         <ProjectsPage />
                         {/* <Routes>
                             <Route path="/" element={<ProjectsPage />} />
